70d71299b9d0e3640eb0dc2ac0497499aa61d20e
SonMyeongHyeon <thsaudgus72@gmail.com>
2023-07-13 16:01:15 +0900

    Google Colab이 아닌 Visual Studio Code에서 Jupyter 환경에서 학습을 시도. 결과는 파일경로 에러가 계속 나왔고, 확인했을 때 별 문제가 없어 해결 방법을 모색 필요.

--- Python(영상처리)/camdetection/video_processing.py
+++ Python(영상처리)/camdetection/video_processing.py
@@ -31,22 +31,39 @@ def generate_frames():
 
         # 현재 프레임에 대해 YOLO 모델을 실행합니다.
         detections = model(frame)[0]
-
         # 객체 검출 결과에 대해 반복합니다.
         for box in detections.boxes.data.tolist():
             # 예측에 연관된 신뢰도(확률)를 추출합니다.
             confidence = box[4]
             # 신뢰도가 최소 신뢰도보다 작은 경우 약한 검출은 건너뜁니다.
             if float(confidence) < CONFIDENCE_THRESHOLD:
-                continue
+              continue
 
             # 신뢰도가 최소 신뢰도보다 큰 경우, 바운딩 박스 좌표를 가져옵니다.
             xmin, ymin, xmax, ymax = int(box[0]), int(box[1]), int(box[2]), int(box[3])
             # 객체의 클래스 이름을 저장합니다.
             class_name = model.names[int(box[5])]
-            # 바운딩 박스를 프레임에 그립니다.
-            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), GREEN, 2)
-            cv2.putText(frame, class_name, (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, BLAKE, 2)
+            # 객체의 중심점 좌표를 계산합니다.
+            center_x = (xmin + xmax) // 2
+            center_y = (ymin + ymax) // 2
+
+            # 바운딩 박스의 크기를 확대합니다.
+            width = xmax - xmin
+            height = ymax - ymin
+
+            expand_ratio = 1.2  # 확대 비율 조정 가능
+            expanded_width = int(width * expand_ratio)
+            expanded_height = int(height * expand_ratio)
+
+            # 확대한 바운딩 박스의 좌표를 계산합니다.
+            expanded_xmin = max(center_x - expanded_width // 2, 0)
+            expanded_ymin = max(center_y - expanded_height // 2, 0)
+            expanded_xmax = expanded_xmin + expanded_width
+            expanded_ymax = expanded_ymin + expanded_height
+
+            # 확대한 바운딩 박스를 프레임에 그립니다.
+            cv2.rectangle(frame, (expanded_xmin, expanded_ymin), (expanded_xmax, expanded_ymax), GREEN, 2)
+            cv2.putText(frame, class_name, (expanded_xmin, expanded_ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, BLAKE, 2)
                  
         # 초당 프레임 수(FPS)를 계산하고 프레임에 표시합니다.
         end = datetime.datetime.now()
---
+++ YOLO(딥러닝)/YOLOv8/real-1/README.dataset.txt
@@ -0,0 +1,6 @@
+# real > 2023-02-04 5:28am
+https://universe.roboflow.com/licenseplate-nrge5/real-nnbha
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
